a0c2e79f096b940c02d89ce58d629bb98a8c4974
rcfroomin <rcfroomin@gmail.com>
2024-04-08 11:51:22 -0700

    added logic to update database when successful mix of ml to potion occurs

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -19,7 +19,10 @@ class PotionInventory(BaseModel):
 def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int):
     """ """
     print(f"potions delievered: {potions_delivered} order_id: {order_id}")
-
+    for potion in potions_delivered:
+        if potion.potion_type == [0, 100, 0, 0]:
+            with db.engine.begin() as connection:
+                result = connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions = num_green_potions + :quantity ;"), quantity=potion.quantity)
     return "OK"
 
 @router.post("/plan")
