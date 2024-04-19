a9839dee5d9518bb8335c5fd4c2ebdcce5de3d28
rcfroomin <rcfroomin@gmail.com>
2024-04-19 11:43:36 -0700

    changed logic in get_best_value_barrel to only return barrels i can afford and that i wouldn't lose money selling potions made from them for 50 gold

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -49,12 +49,52 @@ def get_value(barrel: Barrel):
     return (barrel.ml_per_barrel / barrel.price)
 
 def get_best_value_barrel(wholesale_catalog: list[Barrel]):
-    best_value = wholesale_catalog[0]
-    for barrel in wholesale_catalog:
-        if get_value(barrel) > get_value(best_value):
-            best_value = barrel
-    return best_value
+    if wholesale_catalog == None or len(wholesale_catalog) == 0:
+        return None
+    else:
+        with db.engine.begin() as connection:
+                cur = connection.execute(sqlalchemy.text("SELECT * from global_inventory;"))
+                row1 = cur.fetchone()
+                num_gold = row1[2]
+    
+        best_value = wholesale_catalog[0]
+    
+        for barrel in wholesale_catalog:
+            if get_value(barrel) > get_value(best_value):
+                best_value = barrel
+        
+        while (num_gold <= best_value.price):
+            wholesale_catalog.remove(best_value) #remove a best barrel if i can't afford it
+            best_value = get_best_value_barrel(wholesale_catalog) # find the next best barrel
+    
+        if (best_value.ml_per_barrel / best_value.price) > 2: # make sure i'm not losing money bc all my potions are 100ml and cost 50 gold
+            return best_value
+        else:
+            get_best_value_barrel(wholesale_catalog.remove(best_value))
 
+def what_ml_do_i_need():
+    with db.engine.begin() as connection:
+        cur = connection.execute(sqlalchemy.text("SELECT * from global_inventory;"))
+        row1 = cur.fetchone()
+        num_green_ml = row1[1]
+        num_red_ml = row1[5]
+        num_blue_ml = row1[6]
+        num_green_potions = row1[0]
+        num_red_potions = row1[3]
+        num_blue_potions = row1[4]
+    
+    total_green = num_green_ml + (num_green_potions * 100)
+    total_red = num_red_ml + (num_red_potions * 100)
+    total_blue = num_blue_ml + (num_blue_potions * 100)
+    total_all = total_green + total_red + total_blue
+    colors_needed = [total_red, total_green, total_blue]
+    
+    for color in colors_needed:
+        if ((color / total_all) > .4):
+            colors_needed.remove(color)
+    
+    return colors_needed
+    
 # Gets called once a day
 @router.post("/plan")
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
@@ -70,12 +110,11 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     print(f"wholesale catalog: {wholesale_catalog}")
     
     best_barrel = get_best_value_barrel(wholesale_catalog)
-    print(f"best barrel in the catalog: {best_barrel}")
-
-    while (num_gold <= best_barrel.price):
-        wholesale_catalog.remove(best_barrel)
-        best_barrel = get_best_value_barrel(wholesale_catalog)
-
+    
+    if best_barrel == None:
+        print("No barrels in the catalog I wouldn't lose money on when selling for 50 gold.")
+        return []
+    
     print(f"best barrel i can afford: {best_barrel}")
 
     if best_barrel.potion_type == [0, 1, 0, 0]:
