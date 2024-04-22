fd311aa34c751d64bab2797dd6b3994070fb50e6
rcfroomin <rcfroomin@gmail.com>
2024-04-22 15:19:19 -0700

    removed hardcoded potion types from bottler

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -15,32 +15,41 @@ class PotionInventory(BaseModel):
     potion_type: list[int]
     quantity: int
 
+def build_inventory():
+    catalog = []
+    with db.engine.begin() as connection:
+        cur = connection.execute(sqlalchemy.text("SELECT potions.item_sku FROM potions;"))
+        potion_list = cur.fetchall()
+        
+        for potion in potion_list:
+            cur = connection.execute(sqlalchemy.text("SELECT potions.red, potions.green, potions.blue, potions.dark FROM potions WHERE potions.item_sku = '" + potion[0] + "';"))
+            potion_type = cur.fetchone()
+            potion_type_list = []
+            for color in potion_type:
+                potion_type_list.append(color)
+            catalog.append([potion[0], potion_type_list])
+
+    return catalog
+
 @router.post("/deliver/{order_id}")
 def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int):
     """ """
     print(f"potions delievered: {potions_delivered} order_id: {order_id}")
+    inv = build_inventory()
     for potion in potions_delivered:
-        if potion.potion_type == [0, 100, 0, 0]:
-            p_quantity = potion.quantity
-            ml_quantity = potion.quantity * 100
-            print(f"successfuly made {p_quantity} potions and used {ml_quantity} ml of green potion.")
-            with db.engine.begin() as connection:
-                result = connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions = num_green_potions + " + str(p_quantity) + ";"))
-                result = connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = num_green_ml - " + str(ml_quantity) + ";"))
-        if potion.potion_type == [100, 0, 0, 0]:
-            p_quantity = potion.quantity
-            ml_quantity = potion.quantity * 100
-            print(f"successfuly made {p_quantity} potions and used {ml_quantity} ml of red potion.")
-            with db.engine.begin() as connection:
-                result = connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = num_red_potions + " + str(p_quantity) + ";"))
-                result = connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = num_red_ml - " + str(ml_quantity) + ";"))
-        if potion.potion_type == [0, 0, 100, 0]:
-            p_quantity = potion.quantity
-            ml_quantity = potion.quantity * 100
-            print(f"successfuly made {p_quantity} potions and used {ml_quantity} ml of blue potion.")
-            with db.engine.begin() as connection:
-                result = connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_blue_potions = num_blue_potions + " + str(p_quantity) + ";"))
-                result = connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_blue_ml = num_blue_ml - " + str(ml_quantity) + ";"))
+        for item in inv:
+            if potion.potion_type == item[1]:
+                quantity = potion.quantity
+                num_red_ml =item[1][0]
+                num_green_ml = item[1][1]
+                num_blue_ml = item[1][2]
+                num_dark_ml = item[1][3]
+                with db.engine.begin() as connection:
+                    result = connection.execute(sqlalchemy.text("UPDATE potions SET inventory = (potions.inventory + " + str(quantity) + ") WHERE potions.item_sku = '" + item[0] + "';"))
+                    result = connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = num_red_ml - " + str(num_red_ml) + ";"))
+                    result = connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = num_green_ml - " + str(num_green_ml) + ";"))
+                    result = connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_blue_ml = num_blue_ml - " + str(num_blue_ml) + ";"))
+                    result = connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_dark_ml = num_dark_ml - " + str(num_dark_ml) + ";"))
     return "OK"
 
 @router.post("/plan")
