a0d18cc7aa5d4f88f7c5c38bb704bda0b0bd033a
rcfroomin <rcfroomin@gmail.com>
2024-04-21 20:01:07 -0700

    almost ready to remove all references of potion types in barrels

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -74,28 +74,28 @@ def get_best_value_barrel(wholesale_catalog: list[Barrel]):
         else:
             return None
 
-def what_ml_do_i_need():
-    with db.engine.begin() as connection:
-        cur = connection.execute(sqlalchemy.text("SELECT * from global_inventory;"))
-        row1 = cur.fetchone()
-        num_green_ml = row1[1]
-        num_red_ml = row1[5]
-        num_blue_ml = row1[6]
-        num_green_potions = row1[0]
-        num_red_potions = row1[3]
-        num_blue_potions = row1[4]
+#def what_ml_do_i_need():
+    #with db.engine.begin() as connection:
+        #cur = connection.execute(sqlalchemy.text("SELECT * from global_inventory;"))
+        #row1 = cur.fetchone()
+        #num_green_ml = row1[1]
+        #num_red_ml = row1[5]
+        #num_blue_ml = row1[6]
+        #num_green_potions = row1[0]
+        #num_red_potions = row1[3]
+        #num_blue_potions = row1[4]
     
-    total_green = num_green_ml + (num_green_potions * 100)
-    total_red = num_red_ml + (num_red_potions * 100)
-    total_blue = num_blue_ml + (num_blue_potions * 100)
-    total_all = total_green + total_red + total_blue
-    colors_needed = [total_red, total_green, total_blue]
+    #total_green = num_green_ml + (num_green_potions * 100)
+    #total_red = num_red_ml + (num_red_potions * 100)
+    #total_blue = num_blue_ml + (num_blue_potions * 100)
+    #total_all = total_green + total_red + total_blue
+    #colors_needed = [total_red, total_green, total_blue]
     
-    for color in colors_needed:
-        if ((color / total_all) > .4):
-            colors_needed.remove(color)
+    #for color in colors_needed:
+        #if ((color / total_all) > .4):
+            #colors_needed.remove(color)
     
-    return colors_needed
+    #return colors_needed
     
 # Gets called once a day
 @router.post("/plan")
@@ -104,10 +104,16 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     with db.engine.begin() as connection:
                 cur = connection.execute(sqlalchemy.text("SELECT * from global_inventory;"))
                 row1 = cur.fetchone()
-                num_green_potions = row1[0]
-                num_red_potions = row1[3]
-                num_blue_potions = row1[4]
                 num_gold = row1[2]
+                cur = connection.execute(sqlalchemy.text("SELECT potions.inventory FROM potions WHERE potions.item_sku = 'Red_Potion';"))
+                red_potion = cur.fetchone()
+                num_red_potions = red_potion[0]
+                cur = connection.execute(sqlalchemy.text("SELECT potions.inventory FROM potions WHERE potions.item_sku = 'Green_Potion';"))
+                green_potion = cur.fetchone()
+                num_green_potions = green_potion[0]
+                cur = connection.execute(sqlalchemy.text("SELECT potions.inventory FROM potions WHERE potions.item_sku = 'Blue_Potion';"))
+                blue_potion = cur.fetchone()
+                num_blue_potions = blue_potion[0]
     
     print(f"wholesale catalog: {wholesale_catalog}")
     
@@ -119,32 +125,13 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     
     print(f"best barrel i can afford: {best_barrel}")
 
-    if best_barrel.potion_type == [0, 1, 0, 0]:
-        if num_green_potions < 10 and num_gold >= best_barrel.price:
-            print(f"want to buy a green barrel: {best_barrel.sku}")
-            return [
-                {
-                    "sku": best_barrel.sku,
-                    "quantity": 1
-                }
-            ]
-
-    if best_barrel.potion_type == [1, 0, 0, 0]:
-        if num_red_potions < 10 and num_gold >= best_barrel.price:
-            print(f"want to buy a red barrel: {best_barrel.sku}")
-            return [
-                {
-                    "sku": best_barrel.sku,
-                    "quantity": 1
-                }
-            ]   
-    if best_barrel.potion_type == [0, 0, 1, 0]:
-        if num_blue_potions < 10 and num_gold >= best_barrel.price:
-            print(f"want to buy a blue barrel: {best_barrel.sku}")
-            return [
-                {
-                    "sku": best_barrel.sku,
-                    "quantity": 1
-                }
-            ]   
-    return []
+    if num_gold >= best_barrel.price:
+        print(f"want to buy {best_barrel.sku}")
+        return [
+            {
+                "sku": best_barrel.sku,
+                "quantity": 1
+            }
+        ]
+    else:
+        return []
