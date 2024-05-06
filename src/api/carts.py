779d2cf3b1aa7e1f856b27bd1b8ba83590277f4f
rcfroomin <rcfroomin@gmail.com>
2024-05-06 08:15:13 -0700

    a

--- src/api/carts.py
+++ src/api/carts.py
@@ -90,7 +90,7 @@ def search_orders(
     if search_page == "":
         search_page = 0
         previous = ""
-        if len(results) >= 10:
+        if len(results) > 5:
             next = "https://csc-365-ccc-webservice.onrender.com/carts/search/?customer_name=" + customer_name + "&potion_sku=" + potion_sku + "&search_page=1&sort_col=" + sort_col + "&sort_order=" + sort_order
         else:
             next = ""
